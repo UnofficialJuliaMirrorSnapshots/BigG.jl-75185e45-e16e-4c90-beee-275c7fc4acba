fe408f7dfc04e3161db12b8df86da3074b040bd0
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-25 10:00:53 +0000

    Snapshot of branch master taken on 2019-09-25T06:00:52.876-04:00 from "https://github.com/UnofficialJuliaMirror/BigG.jl-75185e45-e16e-4c90-beee-275c7fc4acba"

--- src/BigG.jl
+++ src/BigG.jl
@@ -5,78 +5,92 @@ using Mustache
 using Dates
 import YAML
 
-export render_posts, render_index
+export render_all
+
+function render_all(md_Dir, output_Dir, post_Tpl, index_Tpl; head_path="")
+    # atm we're only using these infos
+    titles = []
+    dates = []
+    slugs = []
+    experts = []
+
+    htmls = []
+    head_injection = try
+        read(head_path) |> String
+    catch
+        ""
+    end
+
+    post_tpl = read(post_Tpl) |> String
+    index_tpl = read(index_Tpl) |> String
+
+    for md_File in readdir(md_Dir)
+        if !endswith(md_File, ".md")
+            continue
+        end
+        front_text, body_text = front_body("$md_Dir/$md_File")
+        front_matter = YAML.load(front_text)
+        try
+            front_matter["draft"] == true && continue
+        catch
+        end
+
+        html_txt = body_text |> Markdown.parse |> html
+        render_dict = merge(front_matter, Dict("content" => html_txt,
+                                            "head_injection" => head_injection
+                                            )
+                            )
+        push!(htmls, render(post_tpl, render_dict))
+
+        try
+            push!(titles, front_matter["title"])
+            push!(slugs, front_matter["slug"])
+            push!(dates, front_matter["date_published"])
+        catch
+        end
+
+    end
+
+    order = sortperm(dates, rev=true)
+    for i in order
+        pub_day = dates[i]
+        Y, M, D = pub_day |> year, pub_day |> month, pub_day |> day
+        dest_Dir = output_Dir * "/$Y/$M/$D/"
+        ispath(dest_Dir) || mkpath(dest_Dir)
+        slugs[i] = dest_Dir * slugs[i]
+        open("$(slugs[i]).html", "w") do f
+            write(f, htmls[i])
+        end
+    end
+
+    posts = [Dict("slug" => relpath(slugs[i], output_Dir)*".html", "title" => titles[i]) for i in order]
+    open("$output_Dir/index.html", "w") do f
+        write(f, render(index_tpl, posts=posts))
+    end
+
+end
 
 function front_body(mdpath)
     # split out front matter and body of a .md file
     rawmd = readlines(mdpath)
+    return get_front(rawmd), get_body(rawmd)
+end
+
+function get_front(rawmd)
+    # split out front matter of .md lines
     front_lines = findall(x->x=="---", rawmd)
-    if length(front_lines) >= 2
-        front_text = rawmd[front_lines[1]+1:front_lines[2]-1]
-        front_text = join(front_text, "\n")
-        body_text = rawmd[front_lines[2]+1:end]
-        body_text = join(body_text, "\n")
-    else
-        # in case there is not front matter
-        body_text = join(rawmd, "\n")
-    end
-    return front_text, body_text
+    front_text = rawmd[front_lines[1]+1:front_lines[2]-1]
+    front_text = join(front_text, "\n")
+    return front_text
 end
 
-function md2html(mdpath, tplpath, head_injection="")
-    # render a .md file to .html using template, fill in the {{{content}}}
-    front_text, body_text = front_body(mdpath)
-    meta_data = YAML.load(front_text)
-    html_txt = body_text |> Markdown.parse |> html
-
-    render_data = merge(meta_data, Dict("content" => html_txt,
-                                        "head_injection" => head_injection
-                                        )
-                        )
-    tpl = read(tplpath) |> String
-    return render(tpl, render_data)
+function get_body(rawmd)
+    # split out body of a .md lines
+    front_lines = findall(x->x=="---", rawmd)
+    body_text = rawmd[front_lines[2]+1:end]
+    body_text = join(body_text, "\n")
+    return body_text
 end
 
-# renders all .md files using a template from tpl path to output directory
-function render_posts(md_folder, output_folder, tplpath, head_path="")
-    head_injection = ""
-    if head_path!=""
-        head_injection = read(head_path) |> String
-    end
-    for md_fname in readdir(md_folder)
-        if occursin(".md", md_fname)
-            out_fname = replace(md_fname, "md" => "html")
-            open("$output_folder/$out_fname", "w") do f
-                write(f, md2html("$md_folder/$md_fname",
-                                tplpath,
-                                head_injection
-                                )
-                      )
-            end
-        end
-    end
+# module
 end
-
-function render_index(md_folder, output_folder, tplpath)
-    dates = []
-    bnames = []
-    titles = []
-    for md_fname in readdir(md_folder)
-        if md_fname == "index.html" || !occursin(".md", md_fname)
-            continue
-        end
-        front_text, _ = front_body("$md_folder/$md_fname")
-        front_matter = YAML.load(front_text)
-        date, title = front_matter["date"], front_matter["title"]
-        push!(dates, date)
-        push!(titles, title)
-        push!(bnames, replace(md_fname, ".md" => ".html"))
-    end
-    order = sortperm(dates, rev=true)
-    posts = [Dict("bname" => bnames[i], "title" => titles[i]) for i in order]
-    tpl = read(tplpath) |> String
-    open("$output_folder/index.html", "w") do f
-        write(f, render(tpl, posts=posts))
-    end
-end
-end # module
